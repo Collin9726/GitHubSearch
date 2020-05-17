d9b72c2c17fedb7a0db2fb41cbe1a90ff2d06ad4
collin9726 <owino.collin@gmail.com>
2020-05-17 12:48:36 +0300

    define class for repos from API

--- src/app/models/Repo/repo.ts
+++ src/app/models/Repo/repo.ts
@@ -1,2 +1,23 @@
 export class Repo {
+    repoName:string;
+    owner:string;
+    link:string;
+    description:string;
+    createdAt:string;
+    lastUpdated:string;
+    language:string;
+    license:string;
+    forks:number;
+
+    constructor (){
+        this.repoName='';
+        this.owner='';
+        this.link='';
+        this.description='';
+        this.createdAt='';
+        this.lastUpdated='';
+        this.language='';
+        this.license='';
+        this.forks=0;
+    }
 }
