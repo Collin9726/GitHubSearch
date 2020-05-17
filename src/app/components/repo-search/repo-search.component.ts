8a093e9962bc913b6e5fb27a61142e7f86b902f2
collin9726 <owino.collin@gmail.com>
2020-05-17 13:58:48 +0300

    add code in repo component to pick data from service

--- src/app/components/repo-search/repo-search.component.ts
+++ src/app/components/repo-search/repo-search.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RepoNameInput } from 'src/app/models/RepoNameInput/repo-name-input';
 import { Repo } from 'src/app/models/Repo/repo';
 import { ReposTotalCount } from '../../models/ReposTotalCount/repos-total-count';
+import { SearchRepoService } from 'src/app/services/SearchRepo/search-repo.service';
 
 @Component({
   selector: 'app-repo-search',
@@ -13,8 +14,8 @@ export class RepoSearchComponent implements OnInit {
   searchedRepo=new RepoNameInput();
   searchedRepo1=new RepoNameInput();
   repo:Repo=new Repo();
-  repos:Repo[];
-  totalCount: ReposTotalCount;
+  repos:Repo[]=[];
+  totalCount: ReposTotalCount=new ReposTotalCount();
   //userRepos: UserRepos=new UserRepos();
   //totalCount:number;
 
@@ -22,6 +23,7 @@ export class RepoSearchComponent implements OnInit {
     this.searchedRepo1=this.searchedRepo;
     this.repoService.searchRepo(this.searchedRepo1.repoName);
     this.repos=this.repoService.repos;
+    this.totalCount=this.repoService.totalRepos;
     //let repoNum=this.user.publicRepos;
     //let reposURL=this.user.reposUrl;
     //this.repoService.searchRepos();
@@ -30,10 +32,10 @@ export class RepoSearchComponent implements OnInit {
     this.searchedRepo=new RepoNameInput();
   }
 
-  constructor(private repoService: SearchUserService) { }
+  constructor(private repoService: SearchRepoService) { }
   
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
